fa91a9ccf9e236a84d00f3bab1ad8709c325d304
diana26 <diana.c_colunga@hotmail.com>
2018-12-04 11:38:20 -0600

    Add programs to receive and transmit messages from Lora to another Lora

---
+++ RADIOADD_nodoRX.medidor/RADIOADD_nodoRX.medidor.ino
@@ -0,0 +1,129 @@
+// rf69 demo tx rx.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple addressed, reliable messaging client
+// with the RH_RF69 class. RH_RF69 class does not provide for addressing or
+// reliability, so you should only use RH_RF69  if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example rf69_server.
+// Demonstrates the use of AES encryption, setting the frequency and modem 
+// configuration
+
+#include <SPI.h>
+#include <RH_RF69.h>
+#include <RHReliableDatagram.h>
+
+//Radio Setup ------------------------------------------------------------------------/
+
+#define RF69_FREQ 434.0 // Change to 434.0 or 915.0 depending on the radio. (Se supone que en América la frecuencia indicada es 915.0 Hz)
+
+#define MY_ADDRESS     1 // who am i? (server address) (Cada uno de los nodos va a tener una dirección diferente)
+
+#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+  #define RFM69_INT     3  // 
+  #define RFM69_CS      4  //
+  #define RFM69_RST     2  // "A"
+  #define LED           13
+#endif
+
+// Singleton instance of the radio driver
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram rf69_manager(rf69, MY_ADDRESS);
+
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+//-------------------------------------------------------------------------------------/
+void setup() 
+{
+  Serial.begin(9600);
+  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+
+  pinMode(LED, OUTPUT);     
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+
+  Serial.println("Feather Addressed RFM69 RX Test!");
+  Serial.println();
+
+  // manual reset
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
+  
+  if (!rf69_manager.init()) {
+    Serial.println("RFM69 radio init failed");
+    while (1);
+  }
+  Serial.println("RFM69 radio init OK!");
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  // No encryption
+  if (!rf69.setFrequency(RF69_FREQ)) {
+    Serial.println("setFrequency failed");
+  }
+
+  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+  // ishighpowermodule flag set like this:
+  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+  // The encryption key has to be the same as the one in the server
+  uint8_t key[] = "LORENAHERMOSILLO";
+  rf69.setEncryptionKey(key);
+  
+  pinMode(LED, OUTPUT);
+  pinMode(9, OUTPUT);
+
+  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+}
+
+// Dont put this on the stack:
+uint8_t data[] = "And hello back to you";
+// Dont put this on the stack:
+uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+
+//-------------------------------------------------------------------------------------/
+void loop() {
+  if (rf69_manager.available())
+  {
+    Serial.println("Disponible");
+    // Wait for a message addressed to us from the client
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (rf69_manager.recvfromAck(buf, &len, &from)) {
+      buf[len] = 0; // zero out remaining string
+      
+      Serial.print("Got packet from #"); Serial.print(from);
+      Serial.print(" [RSSI :");
+      Serial.print(rf69.lastRssi());
+      Serial.print("] : ");
+      Serial.println((char*)buf);
+     
+       //if para limitar respuesta
+       if (strstr((char *)buf, "Green")) {
+        // Send a reply!
+        uint8_t data[] = "ACK";
+        rf69.send(data, sizeof(data));
+        rf69.waitPacketSent();
+        Serial.println("Sent a reply");
+
+      // Send a reply back to the originator client
+      if (!rf69_manager.sendtoWait(data, sizeof(data), from))
+        Serial.println("Sending failed (no ack)");
+      
+      
+      }
+      else {
+             uint8_t data[] = "error";
+                rf69.send(data, sizeof(data));
+                rf69.waitPacketSent();
+        
+                // Send reply back to the originator client
+                if (!rf69_manager.sendtoWait(data, sizeof(data), from))
+                Serial.println("Sending failed (no ack)");      
+                
+           }
+         }
+      }
+    }
---
+++ RADIOADD_nodoTX.medir/RADIOADD_nodoTX.medir.ino
@@ -0,0 +1,149 @@
+#include "EmonLib.h"
+#include <SPI.h>
+#include <RH_RF69.h>
+#include <RHReliableDatagram.h>
+#include <string.h>
+#include <stdio.h>
+
+//Radio Setup ------------------------------------------------------------------------/
+
+#define RF69_FREQ 434.0 // Change to 434.0 or 915.0 depending on the radio.
+
+#define DEST_ADDRESS   1 // Where to send packets to! (P1-Int Dest Address)
+
+#define MY_ADDRESS     3 // Different for every node
+
+#if defined (__AVR_ATmega328P__)  
+  #define RFM69_INT     3  // 
+  #define RFM69_CS      4  //
+  #define RFM69_RST     2  // "A"
+  #define LED           13
+#endif
+
+// Variables -------------------------------------------------------------------------/
+
+ String RORWS="Green";
+ String RORW;
+ String Fin = "END";
+ String Div = "$";
+ int RORWN;
+ int Vrms = sqrt(125); //Volts
+ double kw; //Potencia  
+ //double kwh;
+ int Ran = random(0 , 9);
+ float t;
+ int x = 1;
+ double Kwh;
+ unsigned long endMillis;
+ unsigned long startMillis; 
+ EnergyMonitor emon1;
+
+
+
+// Radio Initialization---------------------------------------------------------------/
+
+RH_RF69 rf69(RFM69_CS, RFM69_INT); // Singleton instance of the radio driver
+
+RHReliableDatagram rf69_manager(rf69, MY_ADDRESS); // Class to manage message delivery and receipt, using the driver declared above
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+//-------------------------------------------------------------------------------------/
+
+void setup() 
+{
+  Serial.begin(9600);
+  startMillis = millis();
+  int minCurr = 1000;
+  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+  pinMode(LED, OUTPUT);     
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+  
+  Serial.println("Feather Addressed RFM69 TX Test!");
+  Serial.println();
+
+  // manual reset
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
+  endMillis = millis();
+  unsigned long time = endMillis - startMillis;
+  Khw = khW + 
+  if (!rf69_manager.init()) {
+    Serial.println("RFM69 radio init failed");
+    while (1);
+  }
+  Serial.println("RFM69 radio init OK!");
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  // No encryption
+  if (!rf69.setFrequency(RF69_FREQ)) {
+    Serial.println("setFrequency failed");
+  }
+
+  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+  // ishighpowermodule flag set like this:
+  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+  // The encryption key has to be the same as the one in the server
+  uint8_t key[] = "LORENAHERMOSILLO"; //16 caracteres
+  rf69.setEncryptionKey(key);
+  
+  pinMode(LED, OUTPUT);
+  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+
+  emon1.current(1, 111.1);             // Current: input pin, calibration.
+
+}
+
+// Dont put this on the stack:
+uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+uint8_t data[] = "  OK";
+
+//--------------------------------------------------------------------------------------/
+void loop() {
+    
+
+  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  
+  delay(2000);  // Wait 2 seconds between transmits, could also 'sleep' here!
+
+  kw = Irms*V/1000; //kilowatts
+  //kwh = kw*x*t; //kilowatts por hora
+  Serial.println(t);
+  Serial.println(kw);
+  Serial.println(Irms);
+  Serial.println(Vrms);
+ 
+  RORW=RORWS+Div+MY_ADDRESS+Div+DEST_ADDRESS+Div+Ran+Div+Irms+Div+Vrms+Div+kw+Div+Div+Div+Fin;
+
+  
+  char radiopacket[RORW.length()+1];
+  RORW.toCharArray(radiopacket,RORW.length()+1);
+  
+  itoa(packetnum++, radiopacket+60, 10);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  
+  // Send a message to the DESTINATION!
+  if (rf69_manager.sendtoWait((uint8_t *)radiopacket, strlen(radiopacket), DEST_ADDRESS)) {
+    // Now wait for a reply from the server
+    uint8_t len = sizeof(buf);
+    uint8_t from;   
+    if (rf69_manager.recvfromAckTimeout(buf, &len, 2000, &from)) {
+      buf[len] = 0; // zero out remaining string
+      
+      Serial.print("Got reply from #"); Serial.print(from);
+      Serial.print(" [RSSI :");
+      Serial.print(rf69.lastRssi());
+      Serial.print("] : ");
+      Serial.println((char*)buf);     
+      } else {
+      Serial.println("No reply, is anyone listening?");
+    }
+  } else {
+    Serial.println("Sending failed (no ack)");
+  }
+  x = x+1;
+  Serial.println(x);
+  }
